b8db3bd5ebb5b4b351f49ee85d9c8b662713a4cc
nowakpawel <kontakt.nowakpawel@gmail.com>
2024-04-14 15:19:59 +0200

    Initial Project setup

---
+++ src/main/java/com/sportradar/scooreboard/ScooreboardApplication.java
@@ -0,0 +1,13 @@
+package com.sportradar.scooreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScooreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportradar/scooreboard/ScooreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportradar.scooreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScooreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
